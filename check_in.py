1c21a726172f880e8ef53212ff744fd355bb5af5
stormcar <stormcar@dingtalk.com>
2023-06-08 14:20:25 +0800

    修改host

--- check_in.py
+++ check_in.py
@@ -7,7 +7,7 @@ import telegram
 
 
 class GLaDOS_CheckIn:
-    _HOST = "glados.network"
+    _HOST = "glados.rocks"
     _ORIGIN_URL = f"https://{_HOST}"
     _UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
     _BUDGET_DATA_PATH = "budget.json"
